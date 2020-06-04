6771c7e2e7303b02ad9829f831b73c2920b693f9
Marcus Koerner <koerner.marcus@gmail.com>
2020-06-05 01:15:44 +0200

    moved generated css

--- public/app/src/api/base.js
+++ public/app/src/api/base.js
@@ -1,8 +1,8 @@
-//const backend = "http://localhost:8010/api"
-//const wsbackend = "ws://localhost:8010/ws"
+const backend = "http://localhost:8010/api"
+const wsbackend = "ws://localhost:8010/ws"
 
-const backend = "/api"
+//const backend = "/api"
 //TODO: make this configurable
-const wsbackend = "wss://talesofapirate.com/mud/ws"
+//const wsbackend = "wss://talesofapirate.com/mud/ws"
 
 export { backend, wsbackend };
