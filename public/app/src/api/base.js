f6e2c38aeaf9dd782a33c5343526afe3fbb4faae
atla <koerner.marcus@gmail.com>
2020-06-29 23:53:22 +0200

    fixed typo

--- public/app/src/api/base.js
+++ public/app/src/api/base.js
@@ -2,7 +2,7 @@
 //const wsbackend = "ws://localhost:8010/ws"
 
 //TODO: make this configurable
-const backend = "https:/talesofapirate.com/api"
+const backend = "https://talesofapirate.com/api"
 const wsbackend = "wss://talesofapirate.com/ws"
 
 export { backend, wsbackend};
