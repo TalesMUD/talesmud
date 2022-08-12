f1b47b2b643199e9f2337f772b9b9c40560975ba
Marcus Körner <koerner.marcus@gmail.com>
2022-08-13 01:27:32 +0200

    more progress on dialog logic..

--- cmd/dialog_sandbox/main.go
+++ cmd/dialog_sandbox/main.go
@@ -8,6 +8,7 @@ import (
 	d "github.com/talesmud/talesmud/pkg/entities/dialogs"
 
 	"github.com/joho/godotenv"
+	"github.com/manifoldco/promptui"
 )
 
 func main() {
@@ -20,8 +21,57 @@ func main() {
 	d.WrtieToFile(createSampleDialog(), "dialog1.json")
 
 	dialog := d.ReadFromFile("dialog1.json")
+	state := d.DialogState{
+		CurrentDialog: "main",
+		DialogVisited: make(map[string]bool),
+	}
 
-	fmt.Println(dialog.Text)
+	run(dialog, state)
+
+}
+
+func run(dialog *d.Dialog, state d.DialogState) {
+	for {
+		runDialog(dialog, func(key string) {
+			fmt.Printf("You selected: %s\n", dialog.Options[key])
+
+			state.CurrentDialog = dialog.Options[key].ID
+		})
+
+	}
+}
+
+func runDialog(dialog *d.Dialog, choiceFunc func(string)) {
+
+	if dialog.Options == nil {
+		fmt.Println(dialog.Text)
+		return
+	}
+
+	// get string array slice of options texts from dialog
+	options := make([]string, 0)
+	for _, v := range dialog.Options {
+		options = append(options, v.Text)
+	}
+
+	prompt := promptui.Select{
+		Label: dialog.Text,
+		Items: options,
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		log.Error(err)
+	}
+
+	// get ID of option of user selected item and passinto choiceFunc
+	selectedID := ""
+	for _, k := range dialog.Options {
+		if k.Text == result {
+			selectedID = k.ID
+		}
+	}
+	choiceFunc(selectedID)
 
 }
 
@@ -30,9 +80,10 @@ func createSampleDialog() *d.Dialog {
 		d.Options(
 			d.NewDialog("1", "I'm fine, thanks",
 				d.NewResponse("1_1", "Glad to hear!"),
+				d.DOT_SINGLE,
 			),
-			d.NewDialog("2", "I'm doing fine, thanks", nil),
-			d.NewDialog("3", "I'm doing fine, thanks", nil),
+			d.NewDialog("2", "I'm doing fine, thanks", nil, d.DOT_SINGLE),
+			d.NewDialog("3", "I'm doing fine, thanks", nil, d.DOT_SINGLE),
 			d.NewDialogWithRequirements("4", "Until next time",
 				d.NewResponse("4_1", "Bye!"),
 				"1"), // requires visited dialog 1
