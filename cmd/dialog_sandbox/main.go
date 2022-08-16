29674d5f6638fce5c7b12a0bf12d63f536ded779
Marcus Körner <koerner.marcus@gmail.com>
2022-08-17 01:13:04 +0200

    New work on Dialogs
    Added answers, exits and loop breaks

--- cmd/dialog_sandbox/main.go
+++ cmd/dialog_sandbox/main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"reflect"
 
+	//	"reflect"
+
 	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 
 	d "github.com/talesmud/talesmud/pkg/entities/dialogs"
 
+	"github.com/hoisie/mustache"
 	"github.com/joho/godotenv"
 	"github.com/manifoldco/promptui"
 )
@@ -19,12 +23,15 @@ func main() {
 		log.Error("Error loading .env file")
 	}
 
-	d.WrtieToFile(createSampleDialog(), "dialog1.json")
+	d.WrtieToFile(createSampleDialog(), "dialog1.yaml")
 
-	dialog := d.ReadFromFile("dialog1.json")
+	dialog := d.ReadFromFile("dialog1.yaml")
 	state := d.DialogState{
-		CurrentDialog: "main",
+		CurrentDialog: "intro",
 		DialogVisited: make(map[string]bool),
+		Context: map[string]string{
+			"PLAYER": "Hercules",
+		},
 	}
 
 	run(dialog, &state)
@@ -35,13 +42,19 @@ func run(dialog *d.Dialog, state *d.DialogState) {
 
 	currentDialog := dialog
 
-	for {
-		runDialog(currentDialog, state, func(key string) {
-			state.DialogVisited[key] = true
-			state.CurrentDialog = dialog.Options[key].ID
-			currentDialog = dialog.FindDialog(state.CurrentDialog)
-		})
+	for runDialog(currentDialog, state, func(key string) bool {
+		state.DialogVisited[key] = true
+		state.CurrentDialog = key
+		currentDialog = dialog.FindDialog(state.CurrentDialog)
+		if currentDialog.IsDialogExit != nil && *currentDialog.IsDialogExit {
+			return false
+		}
+		return true
+	}) {
+		// ...
 	}
+
+	log.Info("Dialog ended.")
 }
 
 func getAvailableOptions(dialog *d.Dialog, state *d.DialogState) []string {
@@ -59,68 +72,155 @@ func getAvailableOptions(dialog *d.Dialog, state *d.DialogState) []string {
 				}
 			}
 			if requirementsMatched {
-				options = append(options, v.ID)
+				options = append(options, mustache.Render(v.Text, state.Context))
+				// should this go here?
 			}
 
 		} else {
-			options = append(options, v.Text)
+			options = append(options, mustache.Render(v.Text, state.Context))
 		}
 
 	}
 	return options
 }
 
-func runDialog(dialog *d.Dialog, state *d.DialogState, choiceFunc func(string)) {
+func runDialog(dialog *d.Dialog, state *d.DialogState, choiceFunc func(string) bool) bool {
 
+	assert.NotNil(nil, dialog)
+
+	// dialog has no options
 	if dialog.Options == nil {
-		fmt.Println(dialog.Text)
-		choiceFunc("main")
-		return
-	}
+		fmt.Println(mustache.Render(dialog.Text, state.Context))
 
-	if len(dialog.Options) == 1 {
+		if dialog.IsDialogExit != nil && *dialog.IsDialogExit {
+			return choiceFunc("end")
+		} else {
+			return choiceFunc("main")
+		}
+	}
+	// dialog has a single option as a response
+	if dialog.HasAnswer != nil && *dialog.HasAnswer {
 		keys := reflect.ValueOf(dialog.Options).MapKeys()
-		runDialog(dialog.Options[keys[0].String()], state, choiceFunc)
-	} else {
-		options := getAvailableOptions(dialog, state)
-
-		prompt := promptui.Select{
-			Label: dialog.Text,
-			Items: options,
-		}
-
-		_, result, err := prompt.Run()
-		if err != nil {
-			log.Error(err)
-		}
-
-		// get ID of option of user selected item and passinto choiceFunc
-		selectedID := ""
-		for _, k := range dialog.Options {
-			if k.Text == result {
-				selectedID = k.ID
-			}
-		}
-		choiceFunc(selectedID)
+		nextDialog := dialog.Options[keys[0].String()]
+		return runDialog(nextDialog, state, choiceFunc)
 	}
+
+	// dialog has multiple options, get input from user
+	options := getAvailableOptions(dialog, state)
+
+	prompt := promptui.Select{
+		Label: mustache.Render(dialog.Text, state.Context),
+		Items: options,
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		log.Error(err)
+	}
+
+	// get ID of option of user selected item and passinto choiceFunc
+	selectedID := ""
+	for _, k := range dialog.Options {
+		if mustache.Render(k.Text, state.Context) == result {
+			selectedID = k.ID
+		}
+	}
+
+	return choiceFunc(selectedID)
 
 }
 
 func createSampleDialog() *d.Dialog {
-	dialog := d.NewDialog("main", "Hi there how are you doing?",
-		d.Options(
-			d.NewDialog("1", "I'm fine, thanks",
-				d.NewResponse("1_1", "Glad to hear!"),
-				d.DOT_SINGLE,
-			),
-			d.NewDialog("2", "I'm doing fine, thanks", nil, d.DOT_SINGLE),
-			d.NewDialog("3", "Almost great!", nil, d.DOT_SINGLE),
-			d.NewDialogWithRequirements("4", "Until next time",
-				d.NewResponse("4_1", "Bye!"),
-				d.DOT_SINGLE,
-				"1"), // requires visited dialog 1
+
+	return &d.Dialog{
+		ID:   "intro",
+		Text: "Hello stranger, are you visiting Oldtown for the first time?",
+		Options: d.Options(
+			&d.Dialog{
+				ID:        "intro_yes",
+				Text:      "Yes, i go by the name {{PLAYER}}.",
+				HasAnswer: d.RefTrue(),
+				Options: d.Options(
+					&d.Dialog{
+						ID:   "main",
+						Text: "Welcome to Oldtown {{PLAYER}}, let me show you around. How can i help you today?",
+						Options: d.Options(
+							&d.Dialog{
+								ID:        "smith",
+								Text:      "Where can i find the next smith?",
+								HasAnswer: d.RefTrue(),
+								Options: d.Options(
+									&d.Dialog{
+										ID:   "smith_question",
+										Text: "You can find the next smith in the town center on the left side of the road.",
+									},
+								),
+							},
+							&d.Dialog{
+								ID:                     "hidden",
+								Text:                   "You can only see this if you asked me where to find the mayor",
+								RequiresVisitedDialogs: []string{"mayor"},
+								HasAnswer:              d.RefTrue(),
+								Options: d.Options(
+									&d.Dialog{
+										ID:   "hidden_question",
+										Text: "Secretly, i am the Mayor. Dont tell anyone!",
+									},
+								),
+							},
+							&d.Dialog{
+								ID:   "inn",
+								Text: "Where can i grab a drink?",
+							},
+							&d.Dialog{
+								ID:        "mayor",
+								Text:      "Where do i find the mayor of Oldtown?",
+								HasAnswer: d.RefTrue(),
+								Options: d.Options(
+									&d.Dialog{
+										ID:   "mayor_question",
+										Text: "Not sure, why do you ask?",
+									},
+								),
+							},
+							&d.Dialog{
+								ID:   "quests",
+								Text: "Where could i offer my services for some coins?",
+							},
+							&d.Dialog{
+								ID:        "leave",
+								Text:      "I'm done, goodbye.",
+								HasAnswer: d.RefTrue(),
+								Options: d.Options(
+									&d.Dialog{
+										ID:           "end",
+										Text:         "'Til next time {{PLAYER}}.",
+										IsDialogExit: d.RefTrue(),
+									},
+								),
+							},
+						),
+					}),
+			},
 		),
-		d.DOT_ALWAYS,
-	)
-	return dialog
+	}
+	/*
+		dialog := d.NewDialog("intro", "")
+
+		d.NewDialog("main", "Hi there how are you doing?",
+			d.Options(
+				d.NewDialog("1", "I'm fine, thanks",
+					d.NewResponse("1_1", "Glad to hear!"),
+					d.NewShowOnlyOnce(),
+				),
+				d.NewDialog("2", "I'm doing fine, thanks", nil, nil),
+				d.NewDialog("3", "Almost great!", nil, nil),
+				d.NewDialogWithRequirements("4", "Until next time",
+					d.NewResponse("4_1", "Bye!"),
+					d.NewShowOnlyOnce(),
+					"1"), // requires visited dialog 1
+			),
+			nil,
+		)
+		return dialog*/
 }
--- go.mod
+++ go.mod
@@ -14,16 +14,20 @@ require (
 	github.com/gorilla/websocket v1.4.2
 	github.com/hoisie/mustache v0.0.0-20160804235033-6375acf62c69
 	github.com/joho/godotenv v1.3.0
+	github.com/manifoldco/promptui v0.9.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
 	github.com/sirupsen/logrus v1.7.0
+	github.com/stretchr/testify v1.6.1
 	go.mongodb.org/mongo-driver v1.4.2
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
 
 require (
 	github.com/aws/aws-sdk-go v1.34.28 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -38,12 +42,12 @@ require (
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/klauspost/compress v1.9.5 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/manifoldco/promptui v0.9.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
