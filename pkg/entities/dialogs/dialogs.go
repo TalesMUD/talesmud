6d922c898c54ffd87cda833dfa56249e4534bb60
Marcus Körner <koerner.marcus@gmail.com>
2022-08-17 01:13:04 +0200

    New work on Dialogs
    Added answers, exits and loop breaks

--- pkg/entities/dialogs/dialogs.go
+++ pkg/entities/dialogs/dialogs.go
@@ -1,10 +1,10 @@
 package dialogs
 
 import (
-	"encoding/json"
 	"io/ioutil"
 
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
 )
 
 type DialogOptions map[string]*Dialog
@@ -12,6 +12,7 @@ type DialogOptions map[string]*Dialog
 type DialogState struct {
 	CurrentDialog string
 	DialogVisited map[string]bool
+	Context       map[string]string
 }
 
 // create DialogOptionType enum with options SINGLE and ALWAYS
@@ -24,46 +25,41 @@ func NewDialogState() *DialogState {
 	return &DialogState{
 		CurrentDialog: "main",
 		DialogVisited: make(map[string]bool),
+		Context:       make(map[string]string),
 	}
 }
 
 // Dialog ...
 type Dialog struct {
-	ID                     string           `bson:"id,omitempty" json:"id,omitempty"`
-	Text                   string           `bson:"text,omitempty" json:"text,omitempty"`
-	OptionType             DialogOptionType `bson:"dialog_option_type,omitempty" json:"dialog_option_type,omitempty"`
-	Options                DialogOptions    `bson:"options,omitempty" json:"options,omitempty"`
-	RequiresVisitedDialogs []string         `bson:"requiresVisitedDialogs,omitempty" json:"requiresVisitedDialogs,omitempty"`
+	ID                     string        `bson:"id,omitempty" json:"id,omitempty" yaml:"id,omitempty"`
+	Text                   string        `bson:"text,omitempty" json:"text,omitempty" yaml:"text"`
+	Options                DialogOptions `bson:"options,omitempty" json:"options,omitempty" yaml:"options,omitempty"`
+	RequiresVisitedDialogs []string      `bson:"requires_visited_dialogs,omitempty" json:"requires_visited_dialogs,omitempty" yaml:"requires_visited_dialogs,omitempty"`
+	ShowOnlyOnce           *bool         `bson:"show_only_once,omitempty" json:"show_only_once,omitempty" yaml:"show_only_once,omitempty"`
+	HasAnswer              *bool         `bson:"has_answer,omitempty" json:"has_answer,omitempty" yaml:"has_answer,omitempty"`
+	IsDialogExit           *bool         `bson:"is_dialog_exit,omitempty" json:"is_dialog_exit,omitempty" yaml:"is_dialog_exit,omitempty"`
 }
 
 // create a new Dialog
-func NewDialog(id string, text string, options map[string]*Dialog, optionType DialogOptionType) *Dialog {
+func NewDialog(id string, text string, options map[string]*Dialog, showOnlyOnce *bool) *Dialog {
 	return &Dialog{
-		ID:         id,
-		Text:       text,
-		Options:    options,
-		OptionType: optionType,
-	}
+		ID:           id,
+		Text:         text,
+		Options:      options,
+		ShowOnlyOnce: showOnlyOnce}
 }
-func NewDialogWithRequirements(id string, text string, options map[string]*Dialog, optionType DialogOptionType, visited ...string) *Dialog {
+func NewDialogWithRequirements(id string, text string, options map[string]*Dialog, showOnlyOnce *bool, visited ...string) *Dialog {
 	return &Dialog{
 		ID:                     id,
 		Text:                   text,
 		Options:                options,
 		RequiresVisitedDialogs: visited,
-		OptionType:             optionType,
-	}
+		ShowOnlyOnce:           showOnlyOnce}
 }
 
-// creates a dialog that is used as a response (defaults to DOT_ALWAYS) 
+// creates a dialog that is used as a response (defaults to DOT_ALWAYS)
 func NewResponse(id string, text string) DialogOptions {
-	return Options(NewDialog(id, text, nil, DOT_ALWAYS))
-}
-
-func SingleOption(dialog *Dialog) DialogOptions {
-	return DialogOptions{
-		dialog.ID: dialog,
-	}
+	return Options(NewDialog(id, text, nil, nil))
 }
 
 // create dialog map structure and add all parameters to it
@@ -94,7 +90,7 @@ func ReadFromFile(fileName string) *Dialog {
 	data, err := ioutil.ReadFile(fileName)
 	if err == nil {
 		var d Dialog
-		err = json.Unmarshal(data, &d)
+		err = yaml.Unmarshal(data, &d)
 		if err == nil {
 			return &d
 		}
@@ -103,11 +99,16 @@ func ReadFromFile(fileName string) *Dialog {
 }
 
 func WrtieToFile(dialog *Dialog, fileName string) {
-	result, err := json.MarshalIndent(dialog, "", "    ")
+	result, err := yaml.Marshal(dialog) //.MarshalIndent(dialog, "", "    ")
 	if err == nil {
 		err = ioutil.WriteFile(fileName, result, 0644)
 		if err != nil {
-			log.Error("Error writing out.json")
+			log.Error("Error writing %s", fileName)
 		}
 	}
+}
+
+func RefTrue() *bool {
+	b := true
+	return &b
 }
